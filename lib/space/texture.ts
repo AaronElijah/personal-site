1c76681ce94b3076974198a59f7080c0f2da96ae
Aaron Elijah <aaron@octopuswealth.com>
2022-03-05 00:13:57 +0000

    feat: make spaceship controllable from key strokes

--- lib/space/texture.ts
+++ lib/space/texture.ts
@@ -1,42 +1,48 @@
 import * as THREE from 'three'
 import { StaticImages } from '~/lib/utils'
 
-export function randomAsteroidMaterial() {
+export async function randomAsteroidMaterial() {
   const loader = new THREE.TextureLoader()
 
   const decider = Math.random()
   let maps = {}
   if (decider < 0.25) {
     maps = {
-      map: loader.load(StaticImages.crystalMetal.basecolor),
-      aoMap: loader.load(StaticImages.crystalMetal.ao),
-      roughnessMap: loader.load(StaticImages.crystalMetal.roughness),
-      displacementMap: loader.load(StaticImages.crystalMetal.displacement),
-      normalMap: loader.load(StaticImages.crystalMetal.normal),
+      map: await loader.loadAsync(StaticImages.crystalMetal.basecolor),
+      aoMap: await loader.loadAsync(StaticImages.crystalMetal.ao),
+      roughnessMap: await loader.loadAsync(StaticImages.crystalMetal.roughness),
+      displacementMap: await loader.loadAsync(
+        StaticImages.crystalMetal.displacement
+      ),
+      normalMap: await loader.loadAsync(StaticImages.crystalMetal.normal),
     }
   } else if (decider < 0.5) {
     maps = {
-      map: loader.load(StaticImages.lava.basecolor),
-      aoMap: loader.load(StaticImages.lava.ao),
-      roughnessMap: loader.load(StaticImages.lava.roughness),
-      displacementMap: loader.load(StaticImages.lava.displacement),
-      normalMap: loader.load(StaticImages.lava.normal),
+      map: await loader.loadAsync(StaticImages.lava.basecolor),
+      aoMap: await loader.loadAsync(StaticImages.lava.ao),
+      roughnessMap: await loader.loadAsync(StaticImages.lava.roughness),
+      displacementMap: await loader.loadAsync(StaticImages.lava.displacement),
+      normalMap: await loader.loadAsync(StaticImages.lava.normal),
     }
   } else if (decider < 0.75) {
     maps = {
-      map: loader.load(StaticImages.greenCrystal.basecolor),
-      aoMap: loader.load(StaticImages.greenCrystal.ao),
-      roughnessMap: loader.load(StaticImages.greenCrystal.roughness),
-      displacementMap: loader.load(StaticImages.greenCrystal.displacement),
-      normalMap: loader.load(StaticImages.greenCrystal.normal),
+      map: await loader.loadAsync(StaticImages.greenCrystal.basecolor),
+      aoMap: await loader.loadAsync(StaticImages.greenCrystal.ao),
+      roughnessMap: await loader.loadAsync(StaticImages.greenCrystal.roughness),
+      displacementMap: await loader.loadAsync(
+        StaticImages.greenCrystal.displacement
+      ),
+      normalMap: await loader.loadAsync(StaticImages.greenCrystal.normal),
     }
   } else {
     maps = {
-      map: loader.load(StaticImages.lapisLazuli.basecolor),
-      aoMap: loader.load(StaticImages.lapisLazuli.ao),
-      roughnessMap: loader.load(StaticImages.lapisLazuli.roughness),
-      displacementMap: loader.load(StaticImages.lapisLazuli.displacement),
-      normalMap: loader.load(StaticImages.lapisLazuli.normal),
+      map: await loader.loadAsync(StaticImages.lapisLazuli.basecolor),
+      aoMap: await loader.loadAsync(StaticImages.lapisLazuli.ao),
+      roughnessMap: await loader.loadAsync(StaticImages.lapisLazuli.roughness),
+      displacementMap: await loader.loadAsync(
+        StaticImages.lapisLazuli.displacement
+      ),
+      normalMap: await loader.loadAsync(StaticImages.lapisLazuli.normal),
     }
   }
 
