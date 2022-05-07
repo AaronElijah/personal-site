f36e91ad412c95484379c8a92746d6ddb63a0882
AaronElijah <aaronzakelijah@googlemail.com>
2022-05-07 17:48:29 +0100

    feat: add international space station and james webb telescope collectables

---
+++ content/commandModule.md
@@ -0,0 +1,4 @@
+---
+title: Apollo Command Module
+description: Apollo 11 was the first mission in which humans walked on the lunar surface and returned to Earth. On 20 July 1969 two astronauts (Apollo 11 Commander Neil A. Armstrong and LM pilot Edwin E. "Buzz" Aldrin Jr.) landed in Mare Tranquilitatis (the Sea of Tranquility) on the Moon in the Lunar Module (LM) while the Command and Service Module (CSM) (with CM pilot Michael Collins) continued in lunar orbit. During their stay on the Moon, the astronauts set up scientific experiments, took photographs, and collected lunar samples. The LM took off from the Moon on 21 July and the astronauts returned to Earth on 24 July. 
+---
--- lib/scene/objects/models.ts
+++ lib/scene/objects/models.ts
@@ -3,26 +3,22 @@ import { GLTF } from 'three/examples/jsm/loaders/GLTFLoader.js'
 import { clientSideOnly } from '~/lib/utils'
 import { createGLTF, GLTFOptions } from '~/lib/utils/gltf'
 
-interface IAddSpaceship {
+interface IAddGLTFModel {
   scene: THREE.Scene
+  model: GLTFOptions
   options?: {
     scale?: [number, number, number]
     position?: [number, number, number]
   }
 }
 
-export const addStarDestroyer = clientSideOnly(
-  async ({ scene, options }: IAddSpaceship): Promise<[GLTF, THREE.Scene]> => {
-    const gltf = await createGLTF(GLTFOptions.StarDestroyer)
-    if (options?.position) gltf.scene.position.set(...options.position)
-    scene.add(gltf.scene)
-    return [gltf, scene]
-  }
-)
-
-export const addApolloCommandModule = clientSideOnly(
-  async ({ scene, options }: IAddSpaceship): Promise<[GLTF, THREE.Scene]> => {
-    const gltf = await createGLTF(GLTFOptions.ApolloModule)
+export const addGLTF = clientSideOnly(
+  async ({
+    scene,
+    model,
+    options,
+  }: IAddGLTFModel): Promise<[GLTF, THREE.Scene]> => {
+    const gltf = await createGLTF(model)
     if (options?.scale) gltf.scene.scale.set(...options.scale)
     if (options?.position) gltf.scene.position.set(...options.position)
     scene.add(gltf.scene)
